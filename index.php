120b9037d9c17696d7fb887a666e323767eef2de
JAPSBOLS <166400994+JAPSBOLS@users.noreply.github.com>
2024-05-05 19:43:43 +0800

    added admin dashboard

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+  include('includes/header.php');
+  include('includes/navbar.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+  include('includes/footer.php');
+?>

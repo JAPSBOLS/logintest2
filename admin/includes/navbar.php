44c74a22858e6477499399b482811608abfefa6c
Lawrence <94042605+Copeium@users.noreply.github.com>
2024-05-07 16:00:49 +0800

    added logo to navbar

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -3,10 +3,9 @@
 
     <!-- Sidebar - ADD LOGO HERE-->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+        <div class="sidebar-brand-text ml-3 mb-3">
+            <img src='../images/systemLogo.png' width=280></img>
         </div>
-        <div class="sidebar-brand-text mx-3">LAGAY NYO LOGO DITO</div>
     </a>
 
     <!-- Divider -->

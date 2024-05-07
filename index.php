686fd080b6999406f51cb97e226f34341d7f7782
Lawrence <94042605+Copeium@users.noreply.github.com>
2024-05-07 08:32:31 +0800

    ...

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
+    <div class="bg">
+  <?php
+    include('includes/header.php');
+    include('includes/navbar.php');
+    include('includes/footer.php');
+    ?>
+</div>
+
 <style>
   .bg{
     width: 100%;
@@ -8,12 +16,5 @@
     background-image: url(images/BU.jpeg);
     background-size: cover; 
   }
- 
+  
 </style>
-<div class="bg">
-  <?php
-    include('includes/header.php');
-    include('includes/navbar.php');
-    include('includes/footer.php');
-  ?>
-</div>

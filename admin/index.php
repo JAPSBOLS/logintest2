247c6ca3e73eb9532804726eac91be27fac1808f
JAPSBOLS <166400994+JAPSBOLS@users.noreply.github.com>
2024-05-07 12:13:55 +0800

    japs commit

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,9 @@
 <?php
 // Check if user is not logged in or is not an admin, redirect to login page
 session_start();
+include('config/dbconn.php');
 include('includes/header.php');
 include('includes/navbar.php');
-include('config/dbconn.php');
 ?>
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
@@ -183,14 +183,14 @@ include('config/dbconn.php');
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small" ><?php echo $_SESSION['username']?></span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small" ><?php echo $_SESSION['username'] ?></span>
                                 <img class="img-profile rounded-circle"
                                     src="img/undraw_profile.svg">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
+                                <a class="dropdown-item" href="Profile Template/Profile Template/index.html">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                                 </a>

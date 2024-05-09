e187017f39afd1bf1a88adb89d42586b43d12257
Lawrence <94042605+Copeium@users.noreply.github.com>
2024-05-09 21:09:03 +0800

    rename reservation, dashboard

--- admin/index.php
+++ admin/index.php
@@ -68,7 +68,8 @@ include('includes/navbar.php');
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Available for borrowing
+                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                                Available for reservation
                                             </div>
                                             <div class="row no-gutters align-items-center">
                                                 <div class="col-auto">
@@ -98,7 +99,7 @@ include('includes/navbar.php');
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Pending Borrow Requests</div>
+                                                Pending Reservation Requests</div>
                                             <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
                                         </div>
                                         <div class="col-auto">

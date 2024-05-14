e9c591d24ebe7f298813925c979528c46829d297
Eyanny <francisianarevalo0@gmail.com>
2024-05-14 18:29:58 +0800

    Edits on Reservation.php

--- admin/thesisDisplay/thesisdetails.php
+++ admin/thesisDisplay/thesisdetails.php
@@ -35,10 +35,21 @@ if (isset($_GET["Th_Code"])) {
         <div class="card">
             <button class="back-button" onclick="goBack()">Go Back</button>
             <button class="reserve-button" id='reserve-link'>Reserve</button>
-            <script>
-                
-            </script>
 
+            <h2><?php echo $thesis["Th_Title"]; ?></h2>
+            <p>
+                <strong>Status:</strong> <?php echo $thesis["Th_ReservStatus"]; ?><br><br><br>
+                <strong>Code:</strong> <?php echo $thesis["Th_Code"]; ?><br><br><br>
+                <strong>Category:</strong> <?php echo $thesis["Th_Category"]; ?><br><br><br>
+                <strong>Thematic Area:</strong> <?php echo $thesis["Th_ThematicArea"]; ?><br><br><br>
+                <strong>Department:</strong> <?php echo $thesis["Th_Department"]; ?><br><br><br>
+                <strong>Author:</strong> <?php echo $thesis["Authors"]; ?><br><br><br>
+                <strong>Advisor:</strong> <?php echo $thesis["Th_Advisor"]; ?><br><br><br>
+                <strong>Abstract:</strong> <?php echo $thesis["Th_Abstract"]; ?><br><br><br>
+                <strong>Date Modified:</strong> <?php echo $thesis["Th_DateModified"]; ?><br><br><br>
+            </p>
+
+            
             <!-- this is PopUp Rservation -->
                 <div class="popup-container">
                     <div class="popup-content">
@@ -47,8 +58,9 @@ if (isset($_GET["Th_Code"])) {
                                         <input type="number" id="lownumber" value="1000" style="display: none;"/>
                                         <input type="number" id="highnumber" value="9999" style="display: none;" />
                                     <div class="data">
-                                        <div id="randomnumber"></div>
+                                        <div id="randomnumber"><?php echo "Reservation ID:" ; ?></div>
                                     </div>
+                                    <div class="data">User ID: <?php echo $thesis["Th_Code"]; ?></div>
                                     <div class="data">
                                         <label>User Name</label>
                                         <input type="text" required>
@@ -69,18 +81,6 @@ if (isset($_GET["Th_Code"])) {
                                     </form>
                         </div>
                 </div>
-            <h2><?php echo $thesis["Th_Title"]; ?></h2>
-            <p>
-                <strong>Status:</strong> <?php echo $thesis["Th_ReservStatus"]; ?><br><br><br>
-                <strong>Code:</strong> <?php echo $thesis["Th_Code"]; ?><br><br><br>
-                <strong>Category:</strong> <?php echo $thesis["Th_Category"]; ?><br><br><br>
-                <strong>Thematic Area:</strong> <?php echo $thesis["Th_ThematicArea"]; ?><br><br><br>
-                <strong>Department:</strong> <?php echo $thesis["Th_Department"]; ?><br><br><br>
-                <strong>Author:</strong> <?php echo $thesis["Authors"]; ?><br><br><br>
-                <strong>Advisor:</strong> <?php echo $thesis["Th_Advisor"]; ?><br><br><br>
-                <strong>Abstract:</strong> <?php echo $thesis["Th_Abstract"]; ?><br><br><br>
-                <strong>Date Modified:</strong> <?php echo $thesis["Th_DateModified"]; ?><br><br><br>
-            </p>
             
         </div>
     </div>

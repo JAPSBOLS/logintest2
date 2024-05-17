e514dd0f95798dd64130a03dcf6986bbdbb9994a
Lawrence <94042605+Copeium@users.noreply.github.com>
2024-05-17 09:00:42 +0800

    rename add thesis title

--- admin/addThesis/add-thesis.php
+++ admin/addThesis/add-thesis.php
@@ -15,7 +15,7 @@
                 <!-- Begin Content -->
                 <div class="container-fluid">
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                        <h1 class="h3 mb-0 text-gray-800">Add Thesis</h1>
                     </div>
 
                     <!-- <div class="back">

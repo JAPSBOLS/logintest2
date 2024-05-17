bcdc3550075c37701e429bae5f404d4ec0c0075a
JAPSBOLS <166400994+JAPSBOLS@users.noreply.github.com>
2024-05-17 15:54:35 +0800

    trying to add 404 error not finish

---
+++ 404.php
@@ -0,0 +1,20 @@
+<!-- DI PA TO TAPOS -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>404 error</title>
+</head>
+<body>
+
+    <div>
+
+      <p> 404 error</p>
+
+    </div>
+
+  
+</body>
+</html>

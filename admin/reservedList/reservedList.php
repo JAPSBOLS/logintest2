888e2079d7c777a68df79635cc2e67ceb727681f
Eyanny <francisianarevalo0@gmail.com>
2024-05-24 19:44:10 +0800

    inadjust sa reservedlists

--- admin/reservedList/reservedList.php
+++ admin/reservedList/reservedList.php
@@ -17,21 +17,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="reservedList.css">
         <link href="DataTables/datatables.min.css" rel="stylesheet">
-
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <script src="DataTables/datatables.min.js"></script>
         <script src='scripts.js'></script>
         
-    <title></title>
+    <title>Reservation</title>
 </head>
 <body>
     <header>Reserved List</header>
+        <form class="form" action="">
             <table id="myTable" class="display">
                 <thead>
                     <tr class="heading">
                         <th>Thesis Code</th>
-                        <th>User ID</th>
                         <th>Username</th>
                         <th>Reserve Date</th>
                         <th>Return Date</th>
@@ -44,16 +43,18 @@
                     ?>
                     <tr class="data">
                         <td><?php echo $row['Th_Code'];?></td>
-                        <td><?php echo $row['User_ID'];?></td>
                         <td><?php echo $row['User_Name'];?></td>
                         <td><?php echo $row['Reserv_Date'];?></td>
                         <td><?php echo $row['Return_Date'];?></td>
-                        <td><span class="delete-link" onclick="">Available</span></td>
+                        <td><span class="available_link" onclick="">To Available</span>
+                            <span class="notAvailable_link" onclick="">Not Available</span>
+                        </td>
                     </tr>
                     <?php
                     }
                     ?>
                 </tbody>
-            </table>
+        </table>
+        </form>
 </body>
 </html>

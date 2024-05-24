cf5b8cf7f0bd906535712c6bb6793bd0b641a836
Lawrence <94042605+Copeium@users.noreply.github.com>
2024-05-24 20:53:37 +0800

    fixfixfix

--- admin/PendingReservesRequest/decline.php
+++ admin/PendingReservesRequest/decline.php
@@ -10,6 +10,8 @@
         // Delete the thesis record
         $deleteThesis = "DELETE FROM reservation WHERE Reserv_ID = '$Thesis_ID'";
         $runThesisDelete = mysqli_query($conn, $deleteThesis);
+        $updateThesis = "UPDATE thesis SET Th_ReservStatus='Available' WHERE Th_Code = '$Thesis_ID'";
+        $runThesis = mysqli_query($conn, $updateThesis);
         $msg = "Reservation Successfully Declined";
         $_SESSION['msg'] = $msg;
         header('location: index.php');

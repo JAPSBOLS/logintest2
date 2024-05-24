c68958b5a01156da3d4a28e347f3defa543f58cc
KLAWDYO <albanielklyde2@gmail.com>
2024-05-25 01:37:53 +0800

    updated logo and white header in homepage

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
 <div class="container-fluid" style="padding-left: 0px; padding-right: 0px">
-  <nav class="navbar navbar-expand-lg" style="background-color:orange">
+  <nav class="navbar navbar-expand-lg" style="background-color:ivory">
     <div class="container-fluid">
-      <img src='<?php echo GROOT;?>images/systemLogo.png' style="height: 50px; width:300px; object-fit:cover" class='mb-3'></img>
+      <img src='<?php echo GROOT;?>images/BUPLOGO2.png' style="height: 75px; width:200px; object-fit:cover" class='mb-3'></img>
       <a class="navbar-brand" href="#"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -9,16 +9,16 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" id="home" aria-current="page" href="<?php echo GROOT;?>index.php" style="margin-right: 20px;">Home</a>
+            <a class="nav-link" id="home" aria-current="page" href="<?php echo GROOT;?>index.php" style="margin-right: 30px;">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#" style="margin-right: 20px;">Features</a>
+            <a class="nav-link" href="#" style="margin-right: 30px;">Features</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#" style="margin-right: 20px;">Contacts</a>
+            <a class="nav-link" href="#" style="margin-right: 30px;">Contacts</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" id="aboutUS" href="<?php echo GROOT;?>OnePage/aboutUs.php" style="margin-right: 20px;">About Us</a>
+            <a class="nav-link" id="aboutUS" href="<?php echo GROOT;?>OnePage/aboutUs.php" style="margin-right: 30px;">About Us</a>
           </li>
           <li class="nav-item">
             <a class="btn btn-primary" href="<?php echo GROOT;?>Login/newlogin.php">Login</a>

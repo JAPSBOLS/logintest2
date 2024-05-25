379867fa648faf537e22063cc2b3885b350cacc1
JAPSBOLS <166400994+JAPSBOLS@users.noreply.github.com>
2024-05-25 21:24:26 +0800

    pinalitan lng pangalan my reservations

--- admin/my_reservations/my_reservations.php
+++ admin/my_reservations/my_reservations.php
@@ -27,7 +27,7 @@
 
                 <!-- Page Heading -->
                 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                    <h1 class="h3 mb-0 text-gray-800">Reserved Thesis</h1>
+                    <h1 class="h3 mb-0 text-gray-800">My Reservations</h1>
                 </div>
 
                 <div class="card p-3">
